bd7e0d40f0fd2ef97a91345151e843932238c866
Michal Kaczmarek <mkaczmarek@nicmangroup.com>
2020-03-09 04:54:18 -0600

    Init commit continue

---
+++ examples/04/start/js/scripts.js
@@ -0,0 +1,20 @@
+let firstName = "Jan",
+    lastName = "Kowalskiiiii";
+
+let fnName = "getFullName";
+
+let person = {
+    firstName,
+    lastName,
+    [fnName + "1"]: "Witaj!",
+    [fnName]() {
+        return this.firstName + " " + this.lastName;
+    }
+}
+
+// person[fnName] = function(){
+//     return this.firstName + " " + this.lastName;
+// }
+
+
+console.log(person.getFullName());
